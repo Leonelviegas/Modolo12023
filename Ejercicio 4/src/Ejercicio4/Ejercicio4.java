6f4012c88f7ff34fc6baeeb79b40fbc8980fcdf2
Leonel <leonelviegas73@gmail.com>
2023-04-19 14:42:59 -0300

    Ejercicio 4
    Ejercicio 4

---
+++ Ejercicio 4/src/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,37 @@
+
+package Ejercicio4;
+
+
+public class Ejercicio4 {
+
+    
+    public static void main(String[] args) {
+        //Declaro variables
+int N;
+double A;
+char C;
+double suma;
+double resta;
+
+        //Asigno un valor y muestro en pantalla
+N = 20;
+A = 20.25;
+C = 'a';
+    System.out.println("--- Valor variables ---");
+    System.out.println("Variable N = " +N);
+    System.out.println("Variable A = " +A);
+    System.out.println("Variable C = " +C);
+    System.out.println("-----------------------");
+    
+        //Operaciones suma , resta y valor C 
+suma = N + A;
+resta = A - N;
+C = 97;   
+
+    System.out.println("Suma N + A = " +suma);
+    System.out.println("Diferencia A - N = " +resta);
+    System.out.println("Valor C = " +C);
+        
+    }
+    
+}
